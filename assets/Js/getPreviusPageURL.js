8d55ee059475da30fc805ce56a4ed5b75f541375
Yasin Marei <59691385+Yasin3015@users.noreply.github.com>
2023-10-05 01:36:29 +0300

    Update getPreviusPageURL.js

--- assets/Js/getPreviusPageURL.js
+++ assets/Js/getPreviusPageURL.js
@@ -1,9 +1,1 @@
-// /////////////////// save page URL 
 
-let termsLinks = document.querySelectorAll(".open__terms");
-    console.log(termsLinks)
-    termsLinks.forEach(ele=>{
-      ele.addEventListener("mouseenter",()=>{
-        window.localStorage.setItem("URL",window.location)
-      })
-})
